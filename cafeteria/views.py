00d326053a34c45d48019ccc4b415cb7e15fc2ff
Mansaco, Payton <callofthecardinal@gmail.com>
2026-02-27 08:49:49 -0500

    Update views.py

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html', {'form': form})
